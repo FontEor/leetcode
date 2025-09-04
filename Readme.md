5991de3b0970eac30404ac023f22f18d87454ada
刘博 <583139315@qq.com>
2025-09-04 14:50:24 +0800

    feat: 内容更新

--- Readme.md
+++ Readme.md
@@ -2,3 +2,8 @@
 
 1、各种排序算法
 2、promise 相关的算法
+
+# 待复习
+1、keepalive的实现原理
+2、vue2和vue3的diff算法的区别
+3、http2.0和http1.1的区别
--- js/1.js
+++ js/1.js
@@ -1,20 +1,32 @@
-function cloneDeep(value, map = new WeakMap()) {
-  if (value === null || typeof value !== "object") {
-    // 如果是基本数据类型，直接返回
-    return value;
+// 给你一个 n x n 的 方形 整数数组 matrix ，请你找出并返回通过 matrix 的下降路径 的 最小和 。
+
+// 下降路径 可以从第一行中的任何元素开始，并从每一行中选择一个元素。在下一行选择的元素和当前行所选元素最多相隔一列（即位于正下方或者沿对角线向左或者向右的第一个元素）。
+// 具体来说，位置 (row, col) 的下一个元素应当是 (row + 1, col - 1)、(row + 1, col) 或者 (row + 1, col + 1) 。
+// dp[i][j] = Math.min(dp[i - 1][j - 1], dp[i - 1][j], dp[i - 1][j + 1]) + matrix[i][j]
+
+// 输入：matrix = [[2,1,3],[6,5,4],[7,8,9]]
+// 输出：13
+// 解释：如图所示，为和最小的两条下降路径
+var minFallingPathSum = function (matrix) {
+  const n = matrix.length;
+  const dp = new Array(n).fill(0).map(() => new Array(n).fill(0));
+  for(let j=0;j<n;j++){
+    dp[0][j] = matrix[0][j]
   }
-  // 处理循环引用
-  if (map.has(value)) {
-    return map.get(value);
-  }
-  // 创建目标对象或数组
-  const result = Array.isArray(value) ? [] : {};
-  map.set(value, result);
-  // 递归拷贝对象的每个属性
-  for (const key in value) {
-    if (value.hasOwnProperty(key)) {
-      result[key] = cloneDeep(value[key], map);
+  for (let i = 1; i < n; i++) {
+    for (let j = 0; j < n; j++) {
+      if (j === 0) {
+        dp[i][j] = Math.min(dp[i - 1][j], dp[i - 1][j + 1]) + matrix[i][j];
+      } else if (j === n - 1) {
+        dp[i][j] = Math.min(dp[i - 1][j - 1], dp[i - 1][j]) + matrix[i][j];
+      } else {
+        dp[i][j] =
+          Math.min(dp[i - 1][j - 1], dp[i - 1][j], dp[i - 1][j + 1]) +
+          matrix[i][j];
+      }
     }
   }
-  return result;
-}
+  return Math.min(...dp[n - 1]);
+};
+
+console.log(minFallingPathSum([[2,1,3],[6,5,4],[7,8,9]]))
