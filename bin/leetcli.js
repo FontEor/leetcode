74778fdfafdebd378b66a2fb2044474cb15326a7
FontEor <583139315@qq.com>
2026-03-10 15:01:52 +0800

    feat: 日常提交

--- bin/leetcli.js
+++ bin/leetcli.js
@@ -1,10 +1,8 @@
 #!/usr/bin/env node
-const { Command } = require("commander");
+import { Command } from "commander";
+import helpCommand from "../lib/core/help.js";
+import InitCommand from "../lib/core/init.js";
 const program = new Command();
-const { version } = require("../package.json");
-const helpCommand = require("../lib/core/help").default;
-const InitCommand = require("../lib/core/init").default;
-program.version(version);
 helpCommand(program);
 InitCommand(program);
 
